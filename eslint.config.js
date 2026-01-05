83580ac6669302065fc363637a4db8f480d51264
jjerry-o0o <alfndp25@gmail.com>
2026-01-05 21:47:35 +0900

    chore: eslint 9 버전 작성중

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,55 @@
+// @ts-check
+
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended'
+import importPlugin from 'eslint-plugin-import'
+import tanstack from '@tanstack/eslint-plugin-query'
+import { fixupPluginRules } from "@eslint/compat"
 
-export default defineConfig([
-  globalIgnores(['dist']),
+// typescript-eslint 에서 제공하는 config() 와 @ts-check 주석 사용시 속성에 대한 타입 오류 확인 가능
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...reactPlugin.configs.recommended,
+  ...reactHooks.configs.recommended,
+  ...tanstack.configs.recommended,
+  prettierPluginRecommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    extends: [tseslint.configs.disableTypeChecked]
+  }, // 구성이 적용될 파일 패턴
+  { languageOptions: {  // parser, 전역 변수 등 언어 관련 옵션
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        tsconfigRoot: import.meta.dirname,  // 현재 모듈의 dir 경로
+        project: ["tsconfig.node.json", "tsconfig.app.json"],
+      },
     },
   },
-])
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+    settings: { react: { version: "detect" } },
+    rules: {
+      ...reactPlugin.config["recommended"].rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "warn"
+    }
+  }
+)
